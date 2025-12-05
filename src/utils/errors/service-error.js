dc585e8e2828acccc24ea0b92a9f9c4c1f4c8232
breaker250611 <dhruvtomer65@gmail.com>
2025-12-05 18:56:25 +0530

    added basic boiler plate with some error classes and booking model

---
+++ src/utils/errors/service-error.js
@@ -0,0 +1,16 @@
+const { StatusCodes } = require("http-status-codes");
+
+class ServiceError extends Error {
+  constructor(
+    message = "Something went wrong in the service",
+    explanation = "Service Layer error",
+    statusCode = StatusCodes.INTERNAL_SERVER_ERROR
+  ) {
+    this.name = "ServiceError";
+    this.message = message;
+    this.explanation = explanation;
+    this.statusCode = statusCode;
+  }
+}
+
+module.exports = ServiceError;
---
+++ src/utils/errors/validation-error.js
@@ -0,0 +1,15 @@
+const { StatusCodes } = require("http-status-codes");
+class ValidationError extends Error {
+  constructor(error) {
+    super();
+    let explanation = [];
+
+    error.errors.forEach((err) => {
+      explanation.push(err.message);
+    });
+    this.name = "ValidationError";
+    this.message = "NOt able to validate the data sent by the client";
+    this.explanation = explanation;
+    this.statusCode = StatusCodes.BAD_REQUEST;
+  }
+}
